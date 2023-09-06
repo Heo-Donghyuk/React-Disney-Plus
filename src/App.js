16f9705c2e02d94aed1a706b9d2ec984a32d19f6
Heo-DongHyuk <missko30@pusan.ac.kr>
2023-09-06 12:46:49 +0900

    style: Edit App style

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Nav from "./components/Nav";
 function App() {
   return (
     <div
-      className="App relative min-h-[calc(100vh-250px)] overflow-x-hidden block top-[72px] py-[calc(3.5vw-5px)] 
+      className="App relative min-h-[calc(100vh-250px)] overflow-x-hidden block top-[72px] px-[calc(3.5vw-5px)] 
     after:bg-[url('../public/images/home-background.png')] after:bg-center after:bg-cover after:bg-no-repeat after:bg-fixed 
-    after:absolute after:content-center after:inset-0 after:z-[-1]"
+    after:absolute after:content-center after:inset-0 after:z-[-1] after:h-fit"
     >
       <Nav />
       <Banner />
